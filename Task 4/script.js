231ba0f7e41267c9f0b56cabca9fa680c9aee02d
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2023-12-18 19:04:24 +0200

    Done with Movie class

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,21 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        return this.budget > 1e+8;
+    }
+}
+
+const movie1 = new Movie('The Shawshank Redemption', 'Frank Darabont', 25e+6);
+const movie2 = new Movie('Justice League', 'Zack Snyder', 3e+8);
+
+console.log(movie1.wasExpensive())
+console.log(movie2.wasExpensive())
