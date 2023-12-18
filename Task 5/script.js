f80faff711131dee014d08ce86c101336e5c88f3
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2023-12-18 18:18:05 +0200

    First commit with all tasks

---
+++ Task 5/script.js
@@ -0,0 +1,13 @@
+/* ------------------------------ TASK 5 ---------------------------------------------------
+Sutvarkykite užduoties "Task 5" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
+
+let a = composition(one, four);
+let b = division(four, two);
+let c = substraction(three, two);
+let d = multiplication(five, two);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
